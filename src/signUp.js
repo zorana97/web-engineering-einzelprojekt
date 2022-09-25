b06616cb02e1e22eefa3c9ad818bbf240587d881
Zorana-jvc <zorana.jevtic@stud.fh-campuswien.ac.at>
2022-09-25 17:19:11 +0200

    implemented-form-validation

---
+++ src/signUp.js
@@ -0,0 +1,73 @@
+import "./styles/main.css";
+
+const signUpBtn = document.getElementById("signUpBtn");
+const username = document.getElementById("username");
+const age = document.getElementById("age");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+
+signUpBtn.addEventListener("click", (e) => {
+  validateInputs();
+});
+
+const setError = (element, message) => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector(".error");
+
+  errorDisplay.innerText = message;
+  inputControl.classList.add("error");
+  inputControl.classList.remove("success");
+};
+
+const setSuccess = (element) => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector(".error");
+
+  errorDisplay.innerText = "";
+  inputControl.classList.remove("error");
+  inputControl.classList.add("success");
+};
+
+const isValidEmail = (email) => {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+};
+
+const validateInputs = () => {
+  console.log("validate inputs");
+  const usernameValue = username.value.trim();
+  const ageValue = age.value.trim();
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+
+  if (usernameValue === "") {
+    setError(username, "Username is required");
+  } else {
+    setSuccess(username);
+  }
+
+  if (ageValue === "") {
+    setError(age, "Invalid age");
+  } else if (ageValue < 10) {
+    setError(age, "Must be over 10 years old");
+  } else {
+    setSuccess(age);
+  }
+
+  if (emailValue === "") {
+    setError(email, "Email is required");
+  } else if (!isValidEmail(emailValue)) {
+    setError(email, "Provide a valid email address");
+  } else {
+    setSuccess(email);
+  }
+
+  if (passwordValue === "") {
+    setError(password, "Password is required");
+  } else if (passwordValue.length < 5) {
+    setError(password, "Password must be at least 5 character.");
+  } else {
+    setSuccess(password);
+  }
+};
